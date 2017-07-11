e40c71c6d7cb66742c42b4fc92e770bfc3c7ec99
root <root@controller.(none)>
2017-07-11 09:54:37 +0800

    python script

---
+++ check_MQ_group.py
@@ -0,0 +1,39 @@
+__author__ = 'wang'
+#!/usr/bin/evn python
+import requests
+import re
+import time
+import json
+import socket
+
+ts = int(time.time())
+servername = socket.gethostbyname(socket.gethostname())
+URL = 'http://192.168.50.131:8110/rmq/consumer/consumerProgress.do'
+grouplist = ['MobileGroup','EmailMsgGroup','alog_ALogConsumerGroup','alog_BLogConsumerGroup','BillEsConsumerGroup','WechatMatchReceivedBillInfoGroup','billHandoutConsumerGroup','billHandoutPayInfoConsumerGroup','wechatMatchCouponMessageReceiveServiceImpl','MqGoodsMessageReceiveServiceGroup','deviceInfoChangeServiceGroup','MqUserIdChangedMessageIntoServiceGroup','MqHistoryBillUserIdChanedMessageReceiveServiceGroup','MqWeChatAttentionInfoMessageReceiveServiceImplGroup','electronicEvidenceInfoGroup','consumerGroupBill','consumerGroupBillGlobal','consumerGroupRuleOnce','terminalCollectConsumerGroup','consumerGroupCouponDelivery','MqHistoryBillUserIdChanedMessageForCouponReceiveServiceGroup','billCoupon']
+
+def push_result(custom,mode):
+  payload = [
+     {
+        "endpoint": servername,
+        "metric": "MQ."+custom,
+        "timestamp": ts,
+        "step": 60,
+        "value": int(mode),
+        "counterType": "GAUGE",
+        "tags": "info=MQ",
+     }]
+#  print payload
+  r = requests.post("http://127.0.0.1:1988/v1/push", data=json.dumps(payload))
+for onegroup in grouplist:
+ paygroup = {
+    'groupName': onegroup,
+ }
+ r = requests.post(URL, data=paygroup)
+ text = r.text
+ size = re.findall('p>Diff Total::Diff Total:=(.*?):value</p>',text,re.S)
+ for number in size:
+   if r.status_code == 200:
+      push_result(onegroup,number)
+   else:
+     print 'error'
+
